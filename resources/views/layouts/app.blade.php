178400df208f982d323605b90da673eea3c15346
Shefii-02 <shefii.km@gmail.com>
2025-12-16 18:45:17 +0530

    Tawk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -774,19 +774,19 @@
 
 @stack('scripts')
 <!--Start of Tawk.to Script-->
+<!--Start of Tawk.to Script-->
 <script type="text/javascript">
-    var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-    (function() {
-        var s1 = document.createElement("script"),
-            s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/68d95dc54bdd921950d1e27d/1j68hcj00';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-    })();
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/68d95dc54bdd921950d1e27d/1jcjkp57d';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 </script>
+<!--End of Tawk.to Script-->
 <!--End of Tawk.to Script-->
 
 <script>
