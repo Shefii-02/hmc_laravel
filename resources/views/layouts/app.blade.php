d1d9424a29aedff07ddcd039c9661aa4b65705cd
Shefii-02 <shefii.km@gmail.com>
2025-12-16 18:39:57 +0530

    Tawk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -774,7 +774,7 @@
 
 @stack('scripts')
 <!--Start of Tawk.to Script-->
-{{-- <script type="text/javascript">
+<script type="text/javascript">
     var Tawk_API = Tawk_API || {},
         Tawk_LoadStart = new Date();
     (function() {
@@ -786,7 +786,7 @@
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
     })();
-</script> --}}
+</script>
 <!--End of Tawk.to Script-->
 
 <script>
