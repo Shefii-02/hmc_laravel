0383d95c5059a5983d04d6ec29126736c866a6bd
Shefii-02 <shefii.km@gmail.com>
2025-12-19 16:29:38 +0530

    doctors

--- resources/views/frontend/department.blade.php
+++ resources/views/frontend/department.blade.php
@@ -56,9 +56,7 @@
             </div>
         </div>
         <div class="row g-4">
-            @foreach($doctors ?? [] as $doctor)
-
-
+            @foreach($department->doctors ?? [] as $doctor)
                 <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                     <div class="single-team-member card border-0 shadow-sm">
                         <div class="team-member-bg" style="background-image: url('{{ $doctor['doct_img'] }}'); height:300px; background-size:cover; background-position:center;"></div>
